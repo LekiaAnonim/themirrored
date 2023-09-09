b2892e72981ee947a56e68e6b91f17ca78f2a98c
LekiaAnonim <lekiaprosper@gmail.com>
2023-09-09 10:31:50 +0100

    first commit

---
+++ static/js/main/carousel.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('.post-carousel').slick({
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: false,
+        vertical: true
+
+    });
+});
