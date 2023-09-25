8b1a87f9910d459d3ea18aa2662cf0f985280e09
Lekia Prosper <63610232+LekiaAnonim@users.noreply.github.com>
2023-09-25 11:45:20 +0100

    resolve cloudinary

--- themirrored/settings/base.py
+++ themirrored/settings/base.py
@@ -182,6 +182,7 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = "/static/"
 
+
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
 
