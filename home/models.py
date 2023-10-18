c468007ad236d1810d75727de7e1619c4ad88c8f
LekiaAnonim <lekiaprosper@gmail.com>
2023-10-19 00:37:47 +0100

    reduced number of blogs and videos on home page, and made different authors display

--- home/models.py
+++ home/models.py
@@ -36,10 +36,10 @@ class HomePage(MetadataPageMixin, Page):
     
     def get_context(self, request, *args, **kwargs):
         context = super(HomePage, self).get_context(request, *args, **kwargs)
-        blogs = BlogPage.objects.live()[:8]
+        blogs = BlogPage.objects.live()[5:11]
         recent_blogs = BlogPage.objects.live().order_by('date_created')[:4]
         article_of_the_week = BlogPage.objects.live().filter(article_of_the_week=True).order_by('date_created').first()
-        videos = Video.objects.all().order_by('date_created')[:8]
+        videos = Video.objects.all().order_by('date_created')[:6]
         how_of_the_week = HowPage.objects.live().filter(how_of_the_week=True).order_by('date_created').first()
         word_of_the_week = WeeklyWordPage.objects.live().filter(word_of_the_week=True).order_by('date_created').first()
         categories = Category.objects.live()
