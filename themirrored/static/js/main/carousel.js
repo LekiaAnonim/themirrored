6eb9ed233d25e792ae0faf98e457e461de044f3f
LekiaAnonim <lekiaprosper@gmail.com>
2023-12-08 04:07:22 +0100

    changed the design of the site

--- themirrored/static/js/main/carousel.js
+++ themirrored/static/js/main/carousel.js
@@ -1,15 +1,18 @@
 $(document).ready(function() {
     $('.post-carousel').slick({
-        autoplay: true,
+        // autoplay: true,
         autoplaySpeed: 3000,
-        arrows: false,
-        vertical: true
+        arrows: true,
+        ltf: true,
+        dots: true
 
     });
 });
 $('.blog-carousel').slick({
     ltr: true,
     autoplay: true,
+    arrows: true,
     autoplaySpeed: 4000,
+    accessibility: true,
     dots: true
 });
